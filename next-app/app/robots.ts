9b65a4f02b7b63dd211901dd0188ba84cd9b1109
makosa-irvin <47522340+makosa-irvin@users.noreply.github.com>
2026-08-31 09:40:53 +0300

    seo: enable canonical Next frontend crawling

--- next-app/app/robots.ts
+++ next-app/app/robots.ts
@@ -1,10 +1,9 @@
 import type { MetadataRoute } from 'next';
+import { siteUrl } from '../lib/site';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      disallow: '/',
-    },
+    rules: { userAgent: '*', allow: '/', disallow: ['/admin'] },
+    sitemap: siteUrl('/sitemap.xml'),
   };
 }
